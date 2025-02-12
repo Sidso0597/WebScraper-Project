06c85855b4e1b86ef94b86e84e59dcfde4e18a33
Siddhi Kamat <kamatsiddhi97@gmail.com>
2025-02-12 22:38:59 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebScraper_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
